de00b8366e820a12bbf953369c5f528b928155dd
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-21 18:55:05 +0530

    changes modifications

--- src/Token.js
+++ src/Token.js
@@ -10,17 +10,15 @@ const Token=()=>{
     const handleClick = async () => {
         try {
           // Make a POST request to the Instagram API to exchange the authorization code for an access token
-          const response = oauth.authorizationCode(
-            axios.create(),
-            'https://api.instagram.com/oauth/access_token',
-            '1379077412944454',
-            'd0e3c7c69865b68644e4ec626048db03',
-            'authorization_code',
-            'https://insta-basic.vercel.app/',
-             code
-          );
-          const authCode=await response('AUTHORIZATION CODE')
-          console.log(authCode);
+          const response =await  oauth(axios)({
+            url:'https://api.instagram.com/oauth/access_token',
+            client_id:'1379077412944454',
+            client_secret:'d0e3c7c69865b68644e4ec626048db03',
+            grant_type:'authorization_code',
+            redirect_uri:'https://insta-basic.vercel.app/',
+            code:code,
+        });
+        console.log(response.access_token);
           // Extract the access token from the response
         }
         catch(error)
--- src/oauth.js
+++ src/oauth.js
@@ -1,21 +1,28 @@
 import qs from 'qs'
 
-export default function oauth (axios, { url, ...credentials }) {
-  return (moreCredentials = {}) => {
-    const body = {
-      ...credentials,
-      ...moreCredentials
-    }
-
-    // remove blank scope
-    if ('scope' in body && !body.scope) {
-      delete body.scope
-    }
-
-    return axios({
-      url,
-      method: 'post',
-      data: qs.stringify(body)
-    }).then(res => res.data)
+function oauth(axios) {
+    return (config) => {
+      const { url, ...credentials } = config;
+  
+      const body = {
+        ...credentials,
+      };
+  
+      // remove blank scope
+      if ('scope' in body && !body.scope) {
+        delete body.scope;
+      }
+  
+      return axios({
+        url,
+        method: 'post',
+        data: qs.stringify(body),
+      }).then((res) => res.data);
+    };
   }
-}
+ 
+  
+  
+  
+  
+  
