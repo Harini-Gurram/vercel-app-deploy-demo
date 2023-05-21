8d3f3edf7a0ef9363c5a987d4277a138992e49a3
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-21 15:49:47 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 function App() {
-  const [code,setCode]=useState('');
-  console.log(window.href)
-  const handleClick=(event)=>{
-    console.log(window.href)
-    const searchParams = new URLSearchParams(window.href.search);
-    const code = searchParams.get('code');
-    console.log(code)
-    
-  }
+  const [searchParams, setSearchParams] = useSearchParams();
+  const code=searchParams.get("code")
+  console.log(code)
   return (
     <div className="App">
       <div>
