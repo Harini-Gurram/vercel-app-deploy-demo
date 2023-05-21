3db7cef231bc5bee9e1919ffab9d51646f88b79d
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-21 19:03:33 +0530

    changes modifications

---
+++ src/Express.js
@@ -0,0 +1,35 @@
+// Express.js example
+const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
+
+const app = express();
+
+app.post('/exchange-token', async (req, res) => {
+  try {
+    const { code } = req.body;
+    const response = await axios({
+      method: 'post',
+      url: 'https://api.instagram.com/oauth/access_token',
+      data: qs.stringify({
+        client_id: '1379077412944454',
+        client_secret: 'd0e3c7c69865b68644e4ec626048db03',
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://insta-basic.vercel.app/',
+        code: code,
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error exchanging token:', error);
+    res.status(500).json({ error: 'Failed to exchange token' });
+  }
+});
+
+app.listen(3003, () => {
+  console.log('Server is running on port 3000');
+});
--- src/Token.js
+++ src/Token.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+{/*import axios from "axios";
 import { useLocation } from "react-router-dom"
 import oauth from "./oauth";
 //import oauth from 'axios-oauth-client';
@@ -36,3 +36,32 @@ const Token=()=>{
 }
 
 export default Token
+*/}
+
+
+
+import React from 'react';
+import axios from 'axios';
+
+const Token = () => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.post('http://localhost:3003/exchange-token', {
+        code: 'YOUR_AUTHORIZATION_CODE',
+      });
+
+      console.log(response.data.access_token);
+      // You can store the access_token in state or perform any other required action with it
+    } catch (error) {
+      console.error('Error fetching access token:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Get access token</button>
+    </div>
+  );
+};
+
+export default Token;
