7cd3fd7e2b0939d45e72109207ef81b1ae3d51a1
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-22 13:13:00 +0530

    changes modifications

--- src/Token.js
+++ src/Token.js
@@ -9,9 +9,17 @@ const Token = () => {
     console.log(codepara)
     const handleClick = async () => {
     try {
-      const response=await axios.post('https://api.instagram.com/oauth/access_token&client_id=1379077412944454&client_secret=d0e3c7c69865b68644e4ec626048db03&grant_type=authorization_code&redirect_uri=insta-basic.vercel.app&code=AQAFKy3dwqQXMfDAZRxhMlC77P-JdQC22_rs0QmndNt0F1R1xPKg3Goi44V5uGR6F7pDmCsOxchsF6RFOkxZi75aSv8HqjkQUFJgdFGJOsn61raStW8yKuHN0JUvtRD-9wuIA1bVDNVQA4OmaI9zyAmLW8zlU_xCOWgDaoKsrBe0jbtlp-es_qQVjyfZzQR_EBtmNIiXjmS_p3uQMeWPvsudYx3jEASvvmhrW-u66GFzmQ')
-      .then((res)=>{
-        console.log(res.data)
+        const response = await axios({
+            method: 'post',
+            url: 'https://api.instagram.com/oauth/access_token', // Base URL
+            params: { // Query parameters
+              client_id: '1379077412944454',
+              client_secret: 'd0e3c7c69865b68644e4ec626048db03',
+              grant_type: 'authorization_code',
+              redirect_uri: 'https://insta-basic.vercel.app/',
+              code: codepara,
+            },
+          
       })
       console.log(codepara)
       console.log(response.data.access_token);
