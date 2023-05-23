45726aef21dba72b634bc71598dd1d7a0fcb46e6
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-23 09:33:18 +0530

    changes modifications

--- src/Token.js
+++ src/Token.js
@@ -10,19 +10,21 @@ const Token = () => {
     try {
         const formdata=new FormData();
         formdata.append("code",codepara);
-        const clientID = '1379077412944454';
-        const clientSecret = 'd0e3c7c69865b68644e4ec626048db03';
-        const redirectURI = 'https://insta-basic.vercel.app/';
-
+        formdata.append("client_id",1379077412944454);
+        formdata.append("client_secret",d0e3c7c69865b68644e4ec626048db03);
+        formdata.append("redirect_uri",'https://insta-basic.vercel.app/');
         const requestOptions={
           method: 'POST',
           body: formdata,
           redirect: 'follow'
         }
-        const response = await fetch(`https://api.instagram.com/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${redirectURI}&code=${codepara}`, requestOptions);
-        const data = await response.json();
-        console.log(data.access_token);
-        console.log(codepara + " after click");
+        axios.post('https//api.instagram.com/oauth/access_token', formdata, config)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     } catch (error) {
       console.error('Error fetching access token:', error);
     }
