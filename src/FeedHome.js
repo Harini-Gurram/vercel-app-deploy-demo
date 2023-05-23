1aedeec11ff1f9cc2b6c23af433ba1988cdf6aa9
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-23 15:50:36 +0530

    changes modifications

--- src/FeedHome.js
+++ src/FeedHome.js
@@ -1,7 +1,33 @@
+import { useLocation } from "react-router-dom";
+import Axios from 'axios';
+import {React} from 'react'
+const instaBaseURL = Axios.create({
+    baseURL:'https://graph.instagram.com'
+  });
 const FeedHome=()=>{
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const access_tok = searchParams.get('access_token');
+    const handleClick = async () => {
+        try {
+          const {data} = await instaBaseURL.post('com/me/media', {
+            fields:'id,media_type,media_url,username,timestamp',
+            access_token:access_tok,
+          }, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+        )
+          
+        } catch (error) {
+          console.error('Error fetching access token:', error);
+        }
+      };
+        
     return(
         <div>
-          Hii  
+          <button onClick={handleClick}>Get feed</button>
         </div>
     )
 };
