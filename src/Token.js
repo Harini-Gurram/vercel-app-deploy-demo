d125673fbf89665a7550e43aab536ad18bf584a3
Harini-Gurram <harinigurram2001@gmail.com>
2023-05-23 08:51:21 +0530

    changes modifications

--- src/Token.js
+++ src/Token.js
@@ -10,12 +10,16 @@ const Token = () => {
     try {
         const formdata=new FormData();
         formdata.append("code",codepara);
+        const clientID = '1379077412944454';
+        const clientSecret = 'd0e3c7c69865b68644e4ec626048db03';
+        const redirectURI = 'https://insta-basic.vercel.app/';
+
         var requestOptions={
            method: 'POST',
           body: formdata,
           redirect: 'follow'
         }
-        fetch(`https://api.instagram.com/oauth/access_token&client_id=1379077412944454&client_secret=d0e3c7c69865b68644e4ec626048db03&grant_type=authorization_code&redirect_uri=https://insta-basic.vercel.app/&code=${codepara}`, requestOptions)
+        fetch(`https://api.instagram.com/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${redirectURI}&code=${codepara}`, requestOptions)
       .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
