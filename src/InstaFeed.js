98100e02bb636734b57d262f42bc9fe7d7e9d766
Harini-Gurram <harinigurram2001@gmail.com>
2023-06-09 14:45:38 +0530

    added

--- src/InstaFeed.js
+++ src/InstaFeed.js
@@ -1,9 +1,9 @@
-import { useLocation } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
 import Feed from "./Feed"
 import './FeedHome.css'
 import { useSelector } from "react-redux";
 const InstaFeed=()=>{
-    const location = useLocation();
+    //const location = useLocation();
     //const searchParams = new URLSearchParams(location.search);
     //const feeds = searchParams.get('feeds');
     const feeds=useSelector((state)=>state.feedData);
