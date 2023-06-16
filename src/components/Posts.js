d7869474588bed4cef303418dc69e43d1cfb8e0d
Harini-Gurram <harinigurram2001@gmail.com>
2023-06-16 16:22:59 +0530

    added

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -167,7 +167,7 @@ function Display(props){
             {props.buttonNumber==="0" && (
                 <div>
                     <div style={{marginTop:'1.5rem'}}/> 
-                <StyledImageList marginLeft={5} sx={{ width:'100%',height:'100%'}} cols={3} >
+                <StyledImageList marginLeft={5} sx={{ width:'80%',height:'100%'}} cols={3} >
                 {feeds.map((item,index) => (
                   <ImageListItem key={index} onMouseEnter={()=>handleMouseEnter(index)}
                   onMouseLeave={()=>handleMouseEnter(null)}>
